e57a877492f1ee76c9655e66137f131209d524df
Lucas Rott <lro@exentra.de>
2022-07-14 22:06:03 +0200

    ✨ Add basic schema for rpd

---
+++ backend/hasura/migrations/default/1657828916918_rpd_0.1.0/down.sql
@@ -0,0 +1,13 @@
+
+alter table "rpd"."playlists" rename to "playlist";
+
+
+ALTER TABLE "rpd"."parts" ALTER COLUMN "youtube_id" TYPE integer;
+
+alter table "rpd"."parts" rename column "created_by_id" to "created_by";
+
+DROP TABLE "rpd"."playlist_parts";
+
+DROP TABLE "rpd"."playlist";
+
+DROP TABLE "rpd"."parts";
---
+++ backend/hasura/migrations/default/1657828916918_rpd_0.1.0/up.sql
@@ -0,0 +1,45 @@
+
+
+CREATE TABLE "rpd"."parts" ("id" serial NOT NULL, "created_by" text NOT NULL, "youtube_id" integer NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "start_seconds" integer NOT NULL, "end_seconds" integer NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("created_by") REFERENCES "keycloak"."user_entity"("id") ON UPDATE restrict ON DELETE restrict);
+CREATE OR REPLACE FUNCTION "rpd"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_rpd_parts_updated_at"
+BEFORE UPDATE ON "rpd"."parts"
+FOR EACH ROW
+EXECUTE PROCEDURE "rpd"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_rpd_parts_updated_at" ON "rpd"."parts" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
+CREATE TABLE "rpd"."playlist" ("id" serial NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "name" text NOT NULL, "public" boolean NOT NULL DEFAULT true, "created_by" text NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("created_by") REFERENCES "keycloak"."user_entity"("id") ON UPDATE restrict ON DELETE restrict);
+CREATE OR REPLACE FUNCTION "rpd"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_rpd_playlist_updated_at"
+BEFORE UPDATE ON "rpd"."playlist"
+FOR EACH ROW
+EXECUTE PROCEDURE "rpd"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_rpd_playlist_updated_at" ON "rpd"."playlist" 
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
+CREATE TABLE "rpd"."playlist_parts" ("id" serial NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "part_id" integer NOT NULL, "playlist_id" integer NOT NULL, "position" integer NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("part_id") REFERENCES "rpd"."parts"("id") ON UPDATE restrict ON DELETE cascade, FOREIGN KEY ("playlist_id") REFERENCES "rpd"."playlist"("id") ON UPDATE restrict ON DELETE cascade);
+
+alter table "rpd"."parts" rename column "created_by" to "created_by_id";
+
+ALTER TABLE "rpd"."parts" ALTER COLUMN "youtube_id" TYPE text;
+
+alter table "rpd"."playlist" rename to "playlists";
